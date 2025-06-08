a232c38d54ef6ef9fd3f7d22b61c055f3009db66
Madhur6 <madhur.20eic525@rtu.ac.in>
2025-06-08 14:02:04 +0530

    Some updated comments in SpringSecurityConfiguration.java

--- demo/src/main/java/com/example/Begin/demo/security/SpringSecurityConfiguration.java
+++ demo/src/main/java/com/example/Begin/demo/security/SpringSecurityConfiguration.java
@@ -14,24 +14,31 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration  // By default, SB treats these classes as "singleton" 
 public class SpringSecurityConfiguration {
-	
-//	IOC: inversion of control, 
-	
-//	Bean: singleton
-		// By default, fetched eagerly
+
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-		
+		//	If you don't configure SecurityFilterChain, Spring Security will apply its default security, which protects everything by default.
+
+		// Require authentication for all HTTP requests
 		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
 		
+		// Enable form-based login with default settings
 		http.formLogin(form -> form.defaultSuccessUrl("/todo-list", true));
 //		http.formLogin(withDefaults());
 		
-		http.csrf(csrf -> csrf.disable()); // not for production
+        // Disable CSRF protection (not recommended for production use)
+		http.csrf(csrf -> csrf.disable());
 		
+		// Disable X-Frame-Options to allow embedding in iframes (use cautiously)
 		http.headers(headers -> headers.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()));
-		
+		// X-Frame-Options is an HTTP response header used to control whether a webpage can be embedded inside an <iframe>. 
+		// it helps prevent clickjacking attacks, where a malicious site could embed your site in a hidden or disguised iframe to 
+		// trick users into clicking things they didn’t intend to.
+        // ⭐Risks (why it's dangerous):
+        // 1). it opens your app up to clickjacking attacks.
+        // 2). if users are logged in, malicious sites could trick them into performing actions (like clicking buttons) without their knowledge.
+
 		return http.build();
 	}
 	
